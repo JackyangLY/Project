3741be6fd10a5209603b86dcfdfdaad3b456a973
dorayo <yomingyo@gmail.com>
2016-08-12 20:05:42 +0800

    第一次提交project

---
+++ project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project
+//
+//  Created by 洋洋 on 16/8/12.
+//  Copyright © 2016年 Jack_yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  project
+//
+//  Created by 洋洋 on 16/8/12.
+//  Copyright © 2016年 Jack_yy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
