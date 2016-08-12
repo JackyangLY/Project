175cfc718652fa9c1c7fea7b219e36346949e25b
dorayo <yomingyo@gmail.com>
2016-08-12 20:20:13 +0800

    project添加button2016年08月12日20:20:11

--- project/ViewController.m
+++ project/ViewController.m
@@ -14,14 +14,39 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadBasicSetting];
+}
+#pragma mark 加载默认设置
+-(void)loadBasicSetting
+{
+    [self setupUI];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setupUI
+{
+    UIImageView *imgview = [[UIImageView alloc]init];
+    CGFloat imgviewWidth = self.view.frame.size.width;
+    CGFloat imgviewHeight = self.view.frame.size.height/2;
+    imgview.frame = CGRectMake(0, 0, imgviewWidth, imgviewHeight);
+    [imgview setBackgroundColor:[UIColor cyanColor]];
+    [self.view addSubview:imgview];
+    CGFloat buttonX =imgview.center.x-45;
+    CGFloat buttonY =CGRectGetMaxY(imgview.frame)+20;
+    UIButton *button  = [[UIButton alloc]initWithFrame:CGRectMake(buttonX, buttonY, 100, 30)];
+    [button setTitle:@"点击下载" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button setBackgroundColor:[UIColor yellowColor]];
+    [button addTarget:self action:@selector(btnAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
-
+-(void)btnAction:(UIButton *)button
+{
+    [button setBackgroundColor:[UIColor orangeColor]];
+    [button setTitle:@"正在下载" forState:UIControlStateNormal];
+    button.enabled =!button.enabled;
+}
 @end
